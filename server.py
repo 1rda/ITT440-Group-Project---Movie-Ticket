c06aa817984b8f03f807ef2d566187b90e322ad7
ecstahr <adraazlin@gmail.com>
2022-06-29 16:31:56 +0800

    Add movie menu and simple output

--- server.py
+++ server.py
@@ -6,15 +6,44 @@ from multiprocessing import Process
 def displayMenu(s_sock):
 	s_sock.send(str.encode('This is the menu :D'))
 
+def thor(i):
+	print('Movie: Thor and Love for '+i+ ' seats\n')
+	i=float(i)
+	ans = 8*i
+	print("Total: RM ",ans)
+	return ans
+def tony(i):
+	print('Movie: Iron Man for ' +i+' seats\n')
+	i=float(i)
+	ans = 9*i
+	print("Total: RM ", ans)
+	return ans
+
 def process_start(s_sock):
 	s_sock.send(str.encode('Welcome to Matahari Cinema'))
 
 	while True:
 		displayMenu(s_sock)
+		data = s_sock.recv(2048)
+		data = data.decode('utf-8')
 
-		if(option == 'exit'):
+		try:
+			func, result = data.split(" ",2)
+		except:
+			print(" no data received \n")
 			break
 
+		if (func == '1'):
+			ans=thor(result)
+			#s_sock.send(str.encode('Total is ',ans))
+		elif (func == '2'):
+			ans=tony(result)
+		elif (func  == 'exit'):
+			break
+
+
+		equal ="\nTotal is: RM%s\n"% str(ans)
+		s_sock.send(equal.encode())
 	s_sock.close()
 
 if __name__ == '__main__':
