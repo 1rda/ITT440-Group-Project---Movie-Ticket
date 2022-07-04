60d84613e9b85daf64aff62785f5fb12da187646
ecstahr <adraazlin@gmail.com>
2022-07-05 01:21:56 +0800

    add hall receipt, seat (incomplete)

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import socket
 
 ClientSocket = socket.socket()
 
-host = '192.168.56.101'
+host = '192.168.56.115'
 port = 8888
 
 try:
@@ -16,24 +16,27 @@ print(welcome.decode('utf-8'))
 
 while True:
 
+
+	#Display main menu taken from server
 	menu = ClientSocket.recv(2048)
 	print(menu.decode('utf-8'))
 
+	#Input movie
 	func=input("Movie: ")
 	ClientSocket.send(str.encode(func))
 
-	#print(seat.decode('utf-8'))
-
-	#result=input("Seats: ")
-
 	if (func == '3'):
+		print('Thank you! Come again.')
 		break
 	else:
+
+		#ask for showtime
 		showtime = ClientSocket.recv(2048)
 		print(showtime.decode('utf-8'))
-		option = input('\nPick a showtime: ')
+		option = input('Pick a showtime: ')
 		ClientSocket.send(str.encode(option))
 
+		#ask for hall
 		hallz = ClientSocket.recv(2048)
 		print(hallz.decode('utf-8'))
 		halls = input('\nPick a Hall: ')
@@ -48,6 +51,9 @@ while True:
 		seatsdisplay = ClientSocket.recv(2048)
 		print(seatsdisplay.decode('utf-8'))
 
+		#unavailable seats
+		cannotSeat = ClientSocket.recv(2048)
+
 		#choose seats
 		seats = ClientSocket.recv(2048)
 
@@ -55,4 +61,8 @@ while True:
 			pickedSeat = input(seats.decode('utf-8'))
 			ClientSocket.send(str.encode(pickedSeat))
 
+		recep = ClientSocket.recv(2048)
+		print(recep.decode('utf-8'))
+
 ClientSocket.close()
+
