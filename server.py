9410faeedb6086190f2702d3c78fe86c61174167
macaweena <illegirl666@gmail.com>
2022-07-06 02:04:17 +0800

    time

--- server.py
+++ server.py
@@ -2,24 +2,21 @@
 import socket
 import sys
 import errno
+import time
+from time import ctime
 from multiprocessing import Process
-#seats = [A1, A2, A3, A4, A5, B1, B2, B3, B4, B5, C1, C2, C3, C4, C5, D1, D2, D3, D4, D5, E1, E2, E3, E4, E5]
-seatsTaken = ['lol']
+
+seatsTaken = ['picked']
 
 def seats(s_sock):
 	s_sock.send(str.encode('Number of seat(s): '))
 	numseats = s_sock.recv(2048)
 	s_sock.send(str.encode('----------------[Screen]----------------\n\n\t| A1 | A2 | A3 | A4 | A5 |\n\t| B1 | B2 | B3 | B4 | B5 |\n\t| C1 | C2 | C3 | C4 | C5 |\n\t| D1 | D2 | D3 | D4 | D5 |\n\t| E1 | E2 | E3 | E4 | E5 |\n\n'))
 
-	#takenSeats = ''
-
-	#s_sock.send(str.encode('Unavailable seat(s): ' + takenSeats))
 	s_sock.send(str.encode('Choose your seat(s): '))
 
 	for x in range(int(numseats)):
 		pickedSeat = s_sock.recv(2048)
-		#pickedSeat.decode('utf-8')
-		#seatsTaken.append(pickedSeat.decode('utf-8'))
 		print(seatsTaken[-1])
 
 		print(seatsTaken)
@@ -37,32 +34,11 @@ def seats(s_sock):
 
 	return numseats
 
-'''
-		for y in range(len(seatsTaken)):
-			if (y == len(seatsTaken) - 1):
-				break
-			elif (seatsTaken[-1] != seatsTaken[y]):
-				s_sock.send(str.encode('0'))
-				continue
-			elif (seatsTaken[-1] == seatsTaken[y]):
-				s_sock.send(str.encode(f'Seat {seatsTaken[-1]} is unavailable! Please pick another seat.\n'))
-				seatsTaken.pop(-1)
-				s_sock.send(str.encode('Choose your seat(s): '))
-				pickedSeat = s_sock.recv(2048)
-				seatsTaken.append(pickedSeat.decode('utf-8'))
-				print(seatsTaken[-1])
-				break
-'''
-		#for z in seatsTaken:
-			#takenSeats += seatsTaken
-
-	#for x in seatsTaken:
-		#print(x.decode('utf-8'))
-
 #Printing ticket
 def receipt(s_sock,movie, i):
 
-	s_sock.send(str.encode('\n\n\t=====MATAHARI CINEMA====\n\t Movie: '+movie+'\n\t Total: RM' +str(i)+'\n\t Seat: \n\t======================\n\n\n'))
+	s_sock.send(str.encode('\n\n\t=====MATAHARI CINEMA====\n\t Movie: '+movie+'\n\t Total: RM' +str(i)+'\n\t======================\n\n\n'))
+	s_sock.send(str.encode('\n\nTime: ' + ctime() + '\n\n'))
 
 #Calculating total
 def payment(s_sock, i):
@@ -102,12 +78,6 @@ def process_start(s_sock):
 		func = s_sock.recv(2048)
 		func = func.decode('utf-8')
 
-		'''try:
-			func, result = data.split(" ",2)
-		except:
-			print(" no data received \n")
-			break
-'''
 		if (func == '1'):
 			showtime = thor(s_sock)
 			hall(s_sock)
@@ -117,16 +87,14 @@ def process_start(s_sock):
 		elif (func == '2'):
 			showtime = tony(s_sock)
 			hall(s_sock)
-			seats(s_sock)
-			#payment(s_sock)
-			#ticketGenerator(s_sock)
+			s=seats(s_sock)
+			ans=payment(s_sock, s)
+			receipt(s_sock, "IronMan",ans)
 		#disconnects client
 		elif (func == '3'):
 			print('[-] Client has disconnected')
 			break
 
-		#equal ="\nTotal is: RM%s\n"% str(ans)
-		#s_sock.send(equal.encode())
 	s_sock.close()
 
 if __name__ == '__main__':
